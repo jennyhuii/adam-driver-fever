883a3bbb55430dcc1b253b338646b4679b087446
Jenny <j.hui.0130@gmail.com>
2021-12-24 17:01:47 -0500

    added disclaimer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
 import './App.css';
 import quotes from './quotes/quotes.json';
 import Image from './component/Image';
+
+const SmallText = styled.p`
+  font-size: 0.8rem;
+`
 
 const App = () => {
   const randomNumber = Math.floor(Math.random() * quotes.length);
 
   return (
     <div className="App">
+      <SmallText>Disclaimer: This is a fan page. Not affiliated with Adam Driver.</SmallText>
       <Image />
       <p>
         "{quotes[randomNumber].quote}"
